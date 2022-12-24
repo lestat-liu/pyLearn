bbc37f855561072391c4a8a3872546d967250e9c
xcld <hyflwj@163.com>
2022-12-24 23:49:57 +0800

    hwcommit12242349

--- tasks/day003/review.py
+++ tasks/day003/review.py
@@ -8,7 +8,9 @@
 # 1. 列表
 #   - 列表的增删改查
 # ```python
+
 # 1.2 列表的索引和切片
+
 lit = [1, 3, 5, 7, "hello world", [100, 99, 55], (2, 4, 6), {"d": 4, "K": 5}]
 for show in lit:
     print(show)
@@ -17,7 +19,7 @@ for show in lit:
 print("索引和切片")
 print(
     f"lit[2]:{lit[2]}\nlit[2:]:{lit[2:]}\nlit[:2]:{lit[:2]}\nlit[1:3]:{lit[1:3]}\nlit[2:6:2]:{lit[2:6:2]}\nlit[-2]:{lit[-2]}\n")
-print(f"lit[-4][:2]:{lit[-4][:2]}\n")
+print(f"lit[-4][:2]:{lit[-4][:2]}\n")  # 切片分片
 print(f"lit[-4][1]:{lit[-4][1]}\n")
 first, *rest = [1, 2, 3, 4, 5, 6]
 print(first, rest)
@@ -32,19 +34,106 @@ print(f"product(*L):{product(*L)}\n")
 print(f"product(2,*L[1:]):{product(2, *L[1:])}\n")
 
 # 1.3 列表的增删改查
+
 lst = [4, 5, 7, 2, 1, 3, 9, 6, 8, "番薯"]
 print(f"lst:{lst}\n")
 lst.insert(-1, "萝卜生")  # 将萝卜生插入到-1的位置
 print(lst)
+# lst.index("7", 1, 4)
+K = lst[:9]
+K.sort()
+print(f"sort():{K}")
+K.sort(reverse=True)
+print(f"sort(reverse=True):{K}")
+K.reverse()
+print(f"K.reverse():{K}")
 lst2 = ["咸鱼", "豆豉", "榄角", "姜"]
 lst.extend(lst2)  # 将lst2追加的lst结尾 等价于 +=
-print(lst)
+print(f"lst:{lst}")
 
-#   - 列表的嵌套
+# 1.3.2  删--pop；remove；clear；del
+
+lst.pop()  # 默认删除队尾即最右边的元素
+print(f"pop():{lst}")
+lst.pop(0)  # 删除索引位置的元素
+print(f"pop(0):{lst}")
+lst.remove("咸鱼")
+print(f"remove(\"咸鱼\"):{lst}")  # 按元素内容删
+# lst.remove(10)  # lst.remove(10) ValueError: list.remove(x): x not in list 报错退出
+# print(f"remove(10):{lst}")
+
+# lst.clear()  # 清空列表
+# print(f"clear():{lst}")
+
+# del lst[1:2] # 不知道怎么用，输出结果 不对
+# print(f"del lst[1:2]:{lst}")
+
+# 1.3.3 改--lst[2]=str
+
+# lst[-5] = "木瓜生"
+lst[9] = "木瓜生"
+print(f"lst[9]:{lst}")
+lst[10:] = ["鲍鱼", "龙虾"]
+print(f"lst[10:]:{lst}")
+
+# 1.3.4 查--for循环
+
+CC = [1, 1, 1, 5, 5, 6, 2]
+print(CC)
+print(f"CC.count(1):{CC.count(1)}")
+
+for H in lst:
+    print(H, end=" ")
+
+for index, H in enumerate(lst):
+    print(" index: ", index, "H: ", H)
+
 # 2. 元组和元组的嵌套
+## 元组定义
+
+tup = (1, 3, 5, 7, {"h": 89, "i": 90}, [1, 2, 3, 4, 5], (5, 6, 7, 8, (9, 10, 11, 12)))
+print(tup)
+I = list(tup)
+print(I)
+J = tuple(I)
+print(J)
+connect = tup[:4] + J
+print(connect)
+
+# 索引和切片
+# 和列表类似跳过
+
+# count、index、len
+
+Q = tup.count(1)
+print(f"tup.count(1):{Q}")
+R = tup[6][4].index(11)
+print(f"tup.count(1):{R}")
+
 # 3. range
+for S in range(1, 15, 2):
+    print(f"range(1,15,2):{S}")
+    print()
+for T in range(15, 1, -2):
+    print(f"range(15,1,-2):{T}")
+
 # 4. 字典
+'''
+在dict中key是唯⼀的(可哈希). 在保存的时候, 根据key来计算出⼀个内存地址. 然后将key-value保存在这个地址中.
+已知的可哈希(不可变)的数据类型: int, str, tuple, bool
+不可哈希(可变)的数据类型: list, dict, set
+'''
+Dic = {"Fist": 67, "Select": "select hello from K", "Sale": "True","Source": "[78,98,63,45,21]"}
+#  key相同，后面的会覆盖前面的value
+print(f"Dic:{Dic}")
+
 #    - 字典的增删改查
+# 增
+Dic1 = Dic.copy()  # Dic 的浅拷贝
+print(f"Dic1:{Dic1}")
+Dic["Order"] = "(1,2,3,4,5)"
+print(f"Dic:{Dic}")
+
 #    - 字典的嵌套
 # 5. 集合
 # Python中的集合跟数学上的集合是一致的，不允许有重复元素，而且可以进行交集、并集、差集等运算。
