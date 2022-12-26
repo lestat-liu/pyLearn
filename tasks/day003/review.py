47b0170e136a260a27620c76457d4e5f782b51e3
xcld <hyflwj@163.com>
2022-12-26 17:06:58 +0800

    hwcommit122261706

--- tasks/day003/review.py
+++ tasks/day003/review.py
@@ -219,61 +219,68 @@ print("Dic['Source'][2][5][3].get(45):", Dic["Source"][2][5][3].get(45))
 Dic["Source"][2][5][3]["45"] = 125  # 有疑问，为啥没改变45键的值
 print("Dic['Source'][2][5][3].get(45):", Dic["Source"][2][5][3].get(45))
 
-
-for ke,vl in Dic3.items():
+for ke, vl in Dic3.items():
     print(ke, " ", vl)
 
 # 5. 集合
+print("集合")
 # Python中的集合跟数学上的集合是一致的，不允许有重复元素，而且可以进行交集、并集、差集等运算。
 #  ![img.png](img.png)
 #    - 集合增删改查
 #    ```Python
 # # 创建集合的字面量语法
-# set1 = {1, 2, 3, 3, 3, 2}
-# print(set1)
-# print('Length =', len(set1))
+set1 = {1, 2, 3, 3, 3, 2}
+print("set1", set1)
+print('Length =', len(set1))
 # # 创建集合的构造器语法(面向对象部分会进行详细讲解)
-# set2 = set(range(1, 10))
-# set3 = set((1, 2, 3, 3, 2, 1))
-# print(set2, set3)
+set2 = set(range(1, 10))
+set3 = set((1, 2, 3, 3, 2, 1))
+print("set2:", set2, "set3", set3)
 # # 创建集合的推导式语法(推导式也可以用于推导集合)
-# set4 = {num for num in range(1, 100) if num % 3 == 0 or num % 5 == 0}
-# print(set4)
+set4 = {num for num in range(1, 100) if num % 3 == 0 or num % 5 == 0}
+print("set4:", set4)
 # ```
 #
 # 向集合添加元素和从集合删除元素。
 #
 # ```Python
-# set1.add(4)
-# set1.add(5)
-# set2.update([11, 12])
-# set2.discard(5)
-# if 4 in set2:
-#     set2.remove(4)
-# print(set1, set2)
-# print(set3.pop())
-# print(set3)
+print("set1", set1)
+print("set2", set2)
+print("set3", set3)
+set1.add(4)
+set1.add(5)
+set2.update([11, 12])
+set2.discard(5)  # 如果5在set2中就移除
+if 4 in set2:
+    set2.remove(4)  # 如果4在set2中就移除 ,set2为空集 产生keyError
+print("set1:", set1, "set2:", set2)
+print("set3.pop():", set3.pop())
+print("set3.pop():", set3.pop())
+print("set3:", set3)
 # ```
 #    - 集合运算
 # 集合的成员、交集、并集、差集等运算。
 #
 # ```Python
 # # 集合的交集、并集、差集、对称差运算
-# print(set1 & set2)
-# # print(set1.intersection(set2))
-# print(set1 | set2)
-# # print(set1.union(set2))
-# print(set1 - set2)
-# # print(set1.difference(set2))
-# print(set1 ^ set2)
-# # print(set1.symmetric_difference(set2))
-# # 判断子集和超集
-# print(set2 <= set1)
-# # print(set2.issubset(set1))
-# print(set3 <= set1)
-# # print(set3.issubset(set1))
-# print(set1 >= set2)
-# # print(set1.issuperset(set2))
-# print(set1 >= set3)
-# # print(set1.issuperset(set3))
+print("set1", set1)
+print("set2", set2)
+print("set3", set3)
+print("set1 & set2:", set1 & set2)  # 交集，返回一个新集合，包括同时在集合Set1和set2中的元素
+print("set1.intersection(set2):", set1.intersection(set2))  # 返回一个交集的新集合
+print("set1 | set2:", set1 | set2)  # 并集，返回一个新集合，包括集合Set1和set2中的所有元素
+print("set1.union(set2):", set1.union(set2))  # 并集
+print("set1 - set2:", set1 - set2)  # 差集，返回一个新集合，包括在集合Set1中但不在集合set2中的元素
+print("set1.difference(set2):", set1.difference(set2))
+print("set1 ^ set2:", set1 ^ set2)  # 补集，返回一个新集合，包括集合Set1和set2中的元素，但不包括同时在集合S和T中的元素
+print("set1.symmetric_difference(set2):", set1.symmetric_difference(set2))
+# 判断子集和超集
+print("set2 <= set1:", set2 <= set1)
+print("set2.issubset(set1):", set2.issubset(set1))
+print("set3 <= set1:", set3 <= set1)
+print("set3.issubset(set1):", set3.issubset(set1))
+print("set1 >= set2:", set1 >= set2)
+print("set1.issuperset(set2):", set1.issuperset(set2))
+print("set1 >= set3:", set1 >= set3)
+print("set1.issuperset(set3):", set1.issuperset(set3))
 # ```
