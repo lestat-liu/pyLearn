6620f670d1c8495dc1d66d4483ec9b6b340b842a
lestat-liu <1349766697@qq.com>
2023-01-10 21:58:26 +0800

    第五章更新

---
+++ 高阶Python/第五章 精确格式化文本/5.1 使用%进行格式化.py
@@ -0,0 +1,20 @@
+
+"""
+    format_str % value   单个值
+    format_str % (values)  后面跟元组放多个值
+"""
+
+a = 10
+b = 2
+print("a=%s, b" %a )
+print("a=%s, b%s" %(a, b) )
+
+
+
+
+
+
+
+
+
+
---
+++ 高阶Python/第五章 精确格式化文本/5.5 format方法介绍.py
@@ -0,0 +1,16 @@
+
+# 按位置输出对应字符串
+print("{}+{}={}".format(2, 2, 4))
+
+# 按索引输出
+print("{1},--{2}, {0}".format("ni", "hao", "a"))
+
+
+# 按变量名输出到对应位置
+name = "小明"
+age = 18
+st = f"名字是：{name}， 年龄：{age}"
+st3 = f"""名字是：{name}， 年龄：{age}"""
+print(st)
+print(st3)
+
