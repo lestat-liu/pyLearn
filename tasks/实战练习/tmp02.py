8bc2e295e8e29b4f5805c40b5233629ccad68385
xcld <hyflwj@163.com>
2023-01-18 11:13:19 +0800

    hwcommit01181113

--- tasks/实战练习/tmp02.py
+++ tasks/实战练习/tmp02.py
@@ -6,76 +6,78 @@
 @Time: 2023/1/15 20:27
 """
 
-import time,json
-
+import time, json
 
 dic = [
     {
-      "alarm_type": "A故障",
-      "time": "2022-12-19 17:16:11",
-      "address": "世界之窗",
-      "id_number": "A8O0002",
-      "异常时间": 158
+        "alarm_type": "A故障",
+        "time": "2022-12-19 17:16:11",
+        "address": "世界之窗",
+        "id_number": "A8O0002",
+        "异常时间": 158
     },
     {
-      "alarm_type": "A故障",
-      "time": "2022-12-19 17:16:11",
-      "address": "天河区",
-      "id_number": "A8O0000",
-      "异常时间": 155
+        "alarm_type": "A故障",
+        "time": "2022-12-19 17:16:11",
+        "address": "天河区",
+        "id_number": "A8O0000",
+        "异常时间": 155
     },
     {
-      "alarm_type": "B故障",
-      "time": "2022-12-19 15:12:52",
-      "address": "世界之窗",
-      "id_number": "A8O0002",
-      "异常时间": 559
+        "alarm_type": "B故障",
+        "time": "2022-12-19 15:12:52",
+        "address": "世界之窗",
+        "id_number": "A8O0002",
+        "异常时间": 559
     },
     {
-      "alarm_type": "B故障",
-      "time": "2022-12-19 15:08:48",
-      "address": "天河区",
-      "id_number": "A8O0000",
-      "异常时间": 312
+        "alarm_type": "B故障",
+        "time": "2022-12-19 15:08:48",
+        "address": "天河区",
+        "id_number": "A8O0000",
+        "异常时间": 312
     },
     {
-      "alarm_type": "C故障",
-      "time": "2022-12-19 15:02:49",
-      "address": "世界之窗",
-      "id_number": "A8O0002",
-      "异常时间": 150
+        "alarm_type": "C故障",
+        "time": "2022-12-19 15:02:49",
+        "address": "世界之窗",
+        "id_number": "A8O0002",
+        "异常时间": 150
     },
     {
-      "alarm_type": "C故障",
-      "time": "2022-12-19 14:42:13",
-      "address": "天河区",
-      "id_number": "A8O0000",
-      "异常时间": 50
+        "alarm_type": "C故障",
+        "time": "2022-12-19 14:42:13",
+        "address": "天河区",
+        "id_number": "A8O0000",
+        "异常时间": 50
     }
-  ]
+]
 
-
-
-all_time = int((time.time()-time.timezone) )% 86400
+all_time = int((time.time() - time.timezone)) % 86400
 result = {}
 for source_i in dic:
+    # print(source_i)
     if source_i['id_number'] in result:
         abnormal_time = result[source_i['id_number']]['总异常时间'] + source_i['异常时间']
         result[source_i['id_number']]['总异常时间'] = abnormal_time
-        result[source_i['id_number']]['{}率'.format(source_i['alarm_type'])] = '{}%'.format(round(source_i['异常时间']/all_time *100,2))
+        result[source_i['id_number']]['正常时间'] = '{}h'.format((all_time - abnormal_time) / 60 / 60)
+        result[source_i['id_number']]['{}率'.format(source_i['alarm_type'])] = '{}%'.format(
+            round(source_i['异常时间'] / all_time * 100, 2))
     else:
-        result[source_i['id_number']]={
+        result[source_i['id_number']] = {
             '总异常时间': source_i['异常时间'],
             '总时间': all_time,
-            'id_number':source_i['id_number'],
-            'address':source_i['address'],
+            'id_number': source_i['id_number'],
+            'address': source_i['address'],
             'begin_time': time.strftime('%Y-%m-%d %H:%M:%S'),
-            '{}率'.format(source_i['alarm_type']):'{}%'.format(round(source_i['异常时间']/all_time *100,2)),
+            '正常时间': '{}h'.format((all_time - source_i['异常时间']) / 60 / 60),
+            '{}率'.format(source_i['alarm_type']): '{}%'.format(source_i['异常时间'] / all_time * 100),
 
         }
 
 for result_i in result:
     all_normal_time = all_time - result[result_i]['总异常时间']
-    result[result_i]['正常率'] = '{}%'.format(round(all_normal_time / all_time *100,2))
+    result[result_i]['正常时间'] = '{}h'.format(round(all_normal_time / 60 / 60), 2)
+    result[result_i]['正常率'] = '{}%'.format(round(all_normal_time / all_time * 100, 2))
 
-print(json.dumps(result,indent=4,ensure_ascii=False))
+print(json.dumps(result, indent=4, ensure_ascii=False))
